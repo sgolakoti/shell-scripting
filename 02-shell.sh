ea830f741501ade7fb8e33f92cf14e2d68da9429
Sankar <sankargolakoti@gmail.com>
2021-07-19 21:35:26 +0530

    2nd shell

--- 02-shell.sh
+++ 02-shell.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-echo -e "/e[031m This is Test color /e[0m"
+echo -e "\e[031m This is Test color \e[0m"
 arth=$((10+30))
 expr=$(wc -l /etc/passwd)
 arr=(111 222 333)
 echo ${arr[1]}
 echo ${arr[2]}
 echo ${arr[0]}
-echo -e "/e[34m end of file/e[0m"
+echo -e "\e[34m end of file\e[0m"
