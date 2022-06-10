3649166f424fc8a2a1bdf78d291244f7a383a820
Sankar <sankargolakoti@gmail.com>
2022-06-10 15:39:29 +0530

    added 5th file

---
+++ 05-shell.sh
@@ -0,0 +1,8 @@
+#to set host name
+export ps1='hostname'
+#to add  variable
+export home=/var/lib
+#to add var to env 
+export $path=$home:$path
+#to add env var permanantly edit following file
+vi $home/.bashprofile
